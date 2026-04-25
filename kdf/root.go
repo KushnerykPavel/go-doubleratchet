11fb11c25bec8fcf140588c7c52535014363f0c5
Pavlo Kushneryk <pavlo.kushneryk@solidgate.com>
2026-04-25 13:29:03 +0200

    feat(main): remove duplication

--- kdf/root.go
+++
@@ -1,7 +0,0 @@
-// Package kdf provides key derivation functions for the Double Ratchet.
-package kdf
-
-const (
-	// RootKeySize is the root key output size (32 bytes for SHA-256).
-	RootKeySize = 32
-)
